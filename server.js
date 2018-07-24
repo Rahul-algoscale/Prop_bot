45aec9f50cb42f4d4ee59816afb505ec23feab0c
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-24 18:06:40 +0530

    created initial models

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/prop_bot');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// Chec for DB errors
+db.on('error', function(err){
+  console.log(err);
+});
 
 //Init App
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,7 +27,16 @@ app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', function(req, res){
-  res.render('index');
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {
+        title:'Articles',
+        articles: articles
+      });
+    }
+  });
 });
 
 
